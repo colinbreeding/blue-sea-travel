3f9a6b4ab3a79974992b980e1de6cb832b332e64
Colin Breeding <colinmbreeding@gmail.com>
2022-01-13 12:30:46 -0500

    add media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,20 @@ function scrollHeader(){
     if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+// Discover
+let discoverSwiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+      rotate: 0,
+    },
+});
+
 // Video
 const videoFile = document.getElementById('video-file')
 const videoButton = document.getElementById('video-button')
